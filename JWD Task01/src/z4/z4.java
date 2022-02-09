502f2ca6a30642f260e428df21b1207f2fc3552b
odaxnotprogramm <50372211+odaxnotprogramm@users.noreply.github.com>
2022-02-09 16:12:59 +0300

    Update z4.java

--- JWD Task01/src/z4/z4.java
+++ JWD Task01/src/z4/z4.java
@@ -11,7 +11,7 @@ public class z4 {
         double y = in.nextDouble();
         if(x >= -2 && x <= 2 && y >= 0 && y <= 4) System.out.println(true);
         else{
-            if(x*x/0.25 + y*y/0.25 == 1) System.out.println(false);
+            if(x*x/0.25 + y*y/0.25 <= 1) System.out.println(false);
             else System.out.println(x >= -4 && x <= 4 && y <= 0 && y >= -4);
         }
     }
