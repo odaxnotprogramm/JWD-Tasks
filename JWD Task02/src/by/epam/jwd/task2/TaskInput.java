c8787b55611bb78bed761c48cd8c2d5bff87862f
odaxnotprogramm <50372211+odaxnotprogramm@users.noreply.github.com>
2022-02-28 10:49:42 +0300

    Update TaskInput.java

--- JWD Task02/src/by/epam/jwd/task2/TaskInput.java
+++ JWD Task02/src/by/epam/jwd/task2/TaskInput.java
@@ -3,11 +3,24 @@ package by.epam.jwd.task2;
 import java.util.Scanner;
 
 public class TaskInput {
-
+    
+    public boolean check(String str){
+        for(int i = 0;i < str.length();i++){
+            if(!Character.isDigit(str.charAt(i)) &&
+               !(str.charAt(i) == '+' || str.charAt(i) == '-' ||
+                 str.charAt(i) == '/' || str.charAt(i) == '*' || str.charAt(i) == ' ')) return false;
+            return true;
+        }
+    }
+    
     public String inputString(String message){
         Scanner scanner = new Scanner(System.in);
         System.out.print(message);
         String input = scanner.nextLine();
+        while(!check(input)){
+            System.out.print("Enter string that contains only (+,-,*,/) and digits!!!");
+            input = scanner.nextLine();
+        }
         return input;
     }
 }
